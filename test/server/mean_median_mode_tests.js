1067e18d671721c8f944e69a0df62041b5a6d77b
Paul Laskowski <pelaskowski@gmail.com>
2014-12-06 15:56:28 -0800

    fixed mocha test failures due to repathing test file

--- test/server/mean_median_mode_tests.js
+++ test/server/mean_median_mode_tests.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var expect = require('chai').expect;
-var mmm = require('../lib/mean_median_mode');
+var mmm = require('../../lib/mean_median_mode');
 
 describe('Mean', function() {
   it('should get an accurate mean', function() {
