8a6ef840c6d4ee32e475da17738e7d0068447af2
Paul Laskowski <pelaskowski@gmail.com>
2014-12-06 15:09:42 -0800

    karma client test setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,17 +7,29 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-karma');
+  
 
   grunt.initConfig({
     jshint: {
-      all: ['*.js', 'lib/**/*.js', 'client/**/*.js', 'test/**/*.js'],
+      all: [
+        '*.js', 'lib/**/*.js', 
+        'client/**/*.js', 
+        'test/server/**/*.js',
+        'test/client/**/*.js'
+      ],
       options: {
         jshintrc: true
       }
     },
 
     jscs: {
-      src: ['*.js', 'lib/**/*.js', 'client/**/*.js', 'test/**/*.js'],
+      src: [
+        '*.js', 'lib/**/*.js', 
+        'client/**/*.js', 
+        'test/server/**/*.js',
+        'test/client/**/*.js'
+      ],
       options: {
         config: '.jscsrc'
       }
@@ -47,11 +59,30 @@ module.exports = function(grunt) {
         options: {
           transform: ['debowerify']
         }
+      },
+      test: {
+        src: ['test/client/**/*.js'],
+        dest: 'test/angular_testbundle.js',
+        options: {
+          transform: ['debowerify']
+        }
+      }
+    },
+
+    karma: {
+      unit: {
+        configFile: 'karma.conf.js'
+      },
+      continuous: {
+        configFile: 'karma.conf.js',
+        singleRun: true,
+        browsers: ['PhantomJS']
       }
     }
   });
 
   grunt.registerTask('build', ['jshint', 'clean', 'browserify:dev', 'copy:dev']);
   grunt.registerTask('test', ['jshint', 'jscs', 'simplemocha']);
+  grunt.registerTask('test:client', ['browserify:test', 'karma:unit']);
   grunt.registerTask('default', ['test']);
 };
--- test/server/mean_median_mode_tests.js
+++ test/server/mean_median_mode_tests.js
@@ -4,19 +4,19 @@ var expect = require('chai').expect;
 var mmm = require('../lib/mean_median_mode');
 
 describe('Mean', function() {
-  it('gets an accurate mean', function() {
+  it('should get an accurate mean', function() {
     expect(mmm.mean([2, 4, 6])).to.eql(4);
   });
 });
 
 describe('Median', function() {
-  it('gets an accurate median', function() {
+  it('should get an accurate median', function() {
     expect(mmm.median([4, 5, 6])).to.eql(5);
   });
 });
 
 describe('Mode', function() {
-  it('gets an accurate mode', function() {
+  it('should get an accurate mode', function() {
     expect(mmm.mode([5, 5, 6, 7])).to.eql(5);
   });
 });
